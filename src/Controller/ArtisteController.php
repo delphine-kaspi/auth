23b46dcb960df0fa170d166eed3b946f8a944d92
Delphine.Kaspi <delphine.kaspi@yahoo.fr>
2026-02-04 09:39:06 +0100

    Update project structure

---
+++ src/Controller/ArtisteController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Artiste;
+use App\Repository\ArtisteRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArtisteController extends AbstractController
+{
+    #[Route('/artistes', name: 'artistes')]
+    public function index(ArtisteRepository $repo): Response
+    {
+        return $this->render('artistes.html.twig', [
+            'artistes' => $repo->findAll(),
+        ]);
+    }
+
+    #[Route('/artiste/{id}', name: 'artiste')]
+    public function show(Artiste $artiste): Response
+    {
+        return $this->render('artiste.html.twig', [
+            'artiste' => $artiste,
+        ]);
+    }
+}
